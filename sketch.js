97f403ddd6b6dbf3fbfaf3fa4278a597aaecf07a
aogera <schoolgakko.only@gmail.com>
2025-09-24 15:47:43 +0900

    リトライ機能の追加、玉の方向の追加

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 let cnt_frame = 0;
 let circles = [];//円iは中心の座標が(circles[4*i],circles[4*i+1])で速度が(circles[4*i+2],circles[4*i+3])
 let freq_cir = 60;
-let radius = 30;
-let speed_x = 0,speed_y = 1;//基本の円の落下速度
-let acc_y = 1/500;//1フレームごとの加速度。同じ円の加速度は0。新しい円の初速度がかわる
+let radius = 15;
+let speed_x = 1,speed_y = 1;//基本の円の落下速度
+let first_speed_x = 1,first_speed_y = 1;//基本の円の落下の初速度
+let acc_x=1/700;acc_y = 1/700;//1フレームごとの加速度。同じ円の加速度は0。新しい円の初速度がかわる
 let alpha = 100;//行と列を選択したときの不透明度
 let raw = [74,75,76,186];//jkl;
 let col = [65,83,68,70]; //asdf
@@ -11,59 +12,88 @@ let is_gameover = 0;
 let score = 0;
 let life = 3;
 let str = ['♡♡♡','♥♡♡','♥♥♡','♥♥♥'];
-let now_key_condition = 0;
 let canvas_height=400,canvas_width=400;
+let over_circle_idx=-1;
+
+let quarter_height=canvas_height/4,quarter_width=canvas_width/4;
 let backet = 100;
+
 function setup() {
-  createCanvas(canvas_height, canvas_width);
+  createCanvas(canvas_width, canvas_height);
+  // createCanvas(windowWidth,windowHeight);
 }
 
-function draw() {
-  cnt_frame++;
-  background(220);
-  fill(0);
-  textSize(40);
-  text(score,10,40);
-  fill(255,0,0,alpha);
-  textSize(30);
-  text(str[life],canvas_width-100,40);
 
-  if(is_gameover){
-    fill(255);
-    textSize(32);
-    text("GAMEOVER",100,200);
-    return;
+function add_circle(){
+    let start_x = floor(random(0,canvas_width/quarter_width))*backet+backet/2;//4つのレーンからランダムに
+    let start_y = floor(random(0,canvas_height/quarter_height))*backet+backet/2;//4つのレーンからランダムに
 
-  }
-  if(cnt_frame%freq_cir==0){
-    let start_x = floor(random(0,canvas_width/backet))*backet+backet/2;//4つのレーンからランダムに
-    circles.push(start_x,10);//出現座標
+    // circles.push(start_x,canvas_height+radius,0,-speed_y);
 
-    //10%で斜めのボールがでる
+    let direction = max(0,floor(random(-5,4)));
+    if(cnt_frame<600) direction=0;
+    // let direction = 3;
+
+    // 10%で斜めのボールがでる
     if(random(0,10)<1){
-      if(start_x<canvas_width/2) circles.push(1,speed_y);
-      else circles.push(-1,speed_y);
+      if(direction==0){
+        circles.push(start_x,-radius);//出現座標
+        if(start_x<canvas_width/2) circles.push(1,speed_y);
+        else circles.push(-1,speed_y);
+      }
+      if(direction==1){
+        circles.push(-radius,start_y);//出現座標
+        if(start_y<canvas_height/2) circles.push(speed_x,1);
+        else circles.push(speed_x,-1);
+      }
+      if(direction==2){
+        circles.push(start_x,canvas_height+radius);//出現座標
+        if(start_x<canvas_width/2) circles.push(1,-speed_y);
+        else circles.push(-1,-speed_y);
+      }
+      if(direction==3){
+        circles.push(canvas_height+radius,start_y);//出現座標
+        if(start_y<canvas_height/2) circles.push(-speed_x,1);
+        else circles.push(-speed_x,-1);
+      }
     }
     else{
-      circles.push(speed_x,speed_y);
+      if(direction==0){
+        circles.push(start_x,-radius);//出現座標
+        circles.push(0,speed_y);
+      }
+      if(direction==1){
+        circles.push(-radius,start_y);//出現座標
+        circles.push(speed_x,0);
+      }
+      if(direction==2){
+        circles.push(start_x,canvas_height+radius);//出現座標
+        circles.push(0,-speed_y);
+      }
+      if(direction==3){
+        circles.push(canvas_height+radius,start_y);//出現座標
+        circles.push(-speed_x,0);
+      }
     }
-  }
+    // else if(cnt_frame>0&&random(0,5)<1){
+    // else if(1){
+    //   circles.push(start_x,canvas_height+radius);//出現座標
+    //   circles.push(0,-speed_y);
+    //   // if(start_x<canvas_width/2) circles.push(0,-speed_y);
+    //   // else circles.push(0,-speed_y);
+    // }
+    // else{
+    //   circles.push(start_x,-radius);
+    //   circles.push(speed_x,speed_y);
+    // }
+}
 
-  //y方向に少しずつ加速する
-  speed_y+=acc_y;
-
-
-
-  //ボールの描画、位置の修正
+function draw_circle(){
   for(let i = 0;i < circles.length;i += 4){
-
-    if(circles[i]==-1&&circles[i+1]==-1){
-      continue;
-    }
 
     //今の座標でぬる
     fill('white');
-    circle(circles[i],circles[i+1],radius);
+    circle(circles[i],circles[i+1],radius*2);
 
     //座標を更新
     circles[i]   += circles[i+2];
@@ -71,24 +101,88 @@ function draw() {
 
     //端っこに行っていたらゲームオーバー
 
-    if(circles[i]>canvas_width+radius||circles[i]<-radius||circles[i+1]>canvas_height+radius||circles[i+1]<-radius){
-      is_gameover = 1;
+    if(circles[i+3]>0&&circles[i+1]>canvas_height) is_gameover=1;
+    if(circles[i+3]<0&&circles[i+1]<0) is_gameover=1;
+    if(circles[i+2]>0&&circles[i]>canvas_width) is_gameover=1;
+    if(circles[i+2]<0&&circles[i]<0) is_gameover=1;
+
+    if(is_gameover&&over_circle_idx==-1){
+      over_circle_idx = i;
     }
     
   }
 
-  //key操作で色をつける
+}
 
+function init(){
+  is_gameover = 0;
+  circles = [];
+  score = 0;
+  life = 3;
+  speed_x=first_speed_x;
+  speed_y=first_speed_y;
+}
+
+function draw() {
+  cnt_frame++;
+  strokeWeight(0);
+  background(220);
+  fill(0);
+  textSize(40);
+  text(score,10,40);
+  fill(255,0,0);
+  textSize(30);
+  text(str[life],canvas_width-100,40);
+  strokeWeight(1);
+
+  if(is_gameover){
+    fill(255,0,0,alpha);
+    circle(circles[over_circle_idx],circles[over_circle_idx+1],radius*2);
+    fill(255);
+    strokeWeight(5);
+    stroke(255,1,0);
+    textSize(52);
+    text("GAMEOVER",canvas_width/5-21,canvas_height/2);
+    strokeWeight(0);
+    textSize(20);
+    fill(0);
+    text("push R",canvas_width/3,canvas_height/4*3);
+    // text(circles[over_circle_idx],30,30);
+    // text(circles[over_circle_idx+1],60,80);
+    stroke(0);
+    strokeWeight(1);
+    return;
+
+  }
+  if(cnt_frame%freq_cir==0){
+    add_circle();
+  }
+
+  //y方向に少しずつ加速する
+  speed_x+=acc_x;
+  speed_y+=acc_y;
+
+
+
+  //ボールの描画、位置の修正
+  draw_circle();
+
+  //key操作で色をつける
+  color_line();
+
+}
+
+function color_line(){
   for(let i=0;i<4;i++){
     if(keyIsDown(raw[i])){
       fill(0,255,0,alpha);
-      rect(i*100,0,100,400);
+      rect(i*quarter_width,0,quarter_width,canvas_height);
     }
   }
   for(let i=0;i<4;i++){
     if(keyIsDown(col[i])){
       fill(255,255,0,alpha);
-      rect(0,100*i,400,100);
+      rect(0,i*quarter_height,canvas_width,quarter_height);
     }
   }
 }
@@ -97,14 +191,14 @@ function keyPressed(){
   for(let i=0;i<4;i++) for(let j=0;j<4;j++){
     if(keyIsDown(raw[i])&&keyIsDown(col[j])&&key==' '){
       fill(255,0,0,alpha);
-      rect(i*100,j*100,100,100);
+      rect(i*quarter_height,j*quarter_width,quarter_height,quarter_width);
       let how_many_erased = 0;
 
       //配列の一部を消しながら探索するので添字は逆順で見る
       for(let k = circles.length-4;k>=0;k-=4){
 
-        if(i*100<=circles[k]&&circles[k]<=i*100+100){
-          if(j*100<=circles[k+1]&&circles[k+1]<=j*100+100){
+        if(i*quarter_width<=circles[k]&&circles[k]<=(i+1)*quarter_width){
+          if(j*quarter_height<=circles[k+1]&&circles[k+1]<=(j+1)*quarter_height){
             circles.splice(k,4);
             how_many_erased++;
             if(how_many_erased==0) score+=10;
@@ -115,11 +209,22 @@ function keyPressed(){
       }
 
       if(how_many_erased==0){
-        life--;
+        if(life>0) life--;
         if(life==0) is_gameover=1;
       }
     }
   }
+
+  if(key=='r'){
+    init();
+  }
+  if(key=='w'){
+    fullscreen(1);
+  }
+  if(key=='q'){
+    fullscreen(0);
+  }
+
 
 }
 
